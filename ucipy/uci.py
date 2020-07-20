7789f7d3a1f1b6d7fa8e63e439fcbe56386086b2
AppDevMichael <33251702+AppDevMichael@users.noreply.github.com>
2020-07-20 12:55:05 +0100

    Adjusted format

--- ucipy/uci.py
+++ ucipy/uci.py
@@ -5,12 +5,18 @@ Created Date: Friday, 17th July 2020 12:49:35 pm
 -----
 Author: Michael O'Connell
 -----
-Last Modified: Monday, 20th July 2020 12:19:27 pm
+Last Modified: Monday, 20th July 2020 12:53:10 pm
 Modified By: Michael O'Connell
 '''
 import os
 import re
 
+
+class Config(object):
+
+    def __init__(self, config, options):
+        self.config = config
+        self.options = options
 
 class ConfigFile():
     """
@@ -33,7 +39,7 @@ class ConfigFile():
         if os.path.exists(filepath):
             self.filepath = filepath
             config_file = open(filepath, 'r')
-            self.configs = []
+            configs = []
             for line in config_file.readlines():
 
                 if(line[0:6] == "config"):
@@ -41,33 +47,36 @@ class ConfigFile():
 
                     config2 = line.split(" ")[1]
 
-                    self.configs.append({"config": [re.sub(
+                    configs.append({"config": [re.sub(
                         r'[^a-zA-Z0-9_.]', '', config), re.sub(r'[^a-zA-Z0-9_.]', '', config2)]})
 
                 elif(line.lstrip().startswith("option")):
-                    if(not "options" in self.configs[len(self.configs)-1]):
-                        self.configs[len(self.configs) -
+                    if(not "options" in configs[len(configs)-1]):
+                        configs[len(configs) -
                                      1].update({"options": {}})
                     option = line.lstrip().split(" ")
 
                     # print(option)
-                    self.configs[len(self.configs)-1]["options"].update(
+                    configs[len(configs)-1]["options"].update(
                         {re.sub(r'[^a-zA-Z0-9_.]', '', option[1]): re.sub(r'[^a-zA-Z0-9_.]', '', option[2])})
             config_file.close()
-            return(self.configs)
+            self.sections = []
+            for i in configs:
+                self.sections.append( Config(i["config"], i["options"]))
+            return(self.sections)
         else:
             return("Error")
 
     def save(self):
 
-        if(self.configs):
+        if(self.sections):
             strout = []
 
-            for config in self.configs:
+            for config in self.sections:
                 strout.append(
-                    "config " + config["config"][1] + " '" + config["config"][0] + "'")
-                for option in config["options"].keys():
-                    strout.append("\toption '%s' '%s'" % (option, config["options"][option]))
+                    "config " + config.config[1] + " '" + config.config[0] + "'")
+                for option in config.options.keys():
+                    strout.append("\toption '%s' '%s'" % (option, config.options[option]))
                 strout.append("\n")
             str = "\n"
             x = str.join(strout)
