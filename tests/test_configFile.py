9ad87edd57fec06083419a48f3382e6c98d75120
AppDevMichael <33251702+AppDevMichael@users.noreply.github.com>
2020-07-20 13:57:10 +0100

    Adjusted config orientation

--- tests/test_configFile.py
+++ tests/test_configFile.py
@@ -5,7 +5,7 @@ Created Date: Friday, 17th July 2020 1:49:45 pm
 -----
 Author: Michael O'Connell
 -----
-Last Modified: Monday, 20th July 2020 12:54:06 pm
+Last Modified: Monday, 20th July 2020 12:56:27 pm
 Modified By: Michael O'Connell
 '''
 
@@ -23,9 +23,8 @@ def test_uci_readfile():
 
 def test_uci_fileParse():
     test = uci.ConfigFile().read_config_file("tests/testconfig") 
-    assert test[2].config[0] == "IRI", "Failed to parse file"
-    assert test[2].config[1] == "telemetry", "Failed to parse file"
-    assert test[2].config[1] == "telemetry", "Failed to parse file"
+    assert test[2].config[0] == "telemetry", "Failed to parse file"
+    assert test[2].config[1] == "IRI", "Failed to parse file"
     assert test[0].options["base_dir"] == 'mntsdcardincoming', "Failed to parse file"
 
 def test_uci_save():
